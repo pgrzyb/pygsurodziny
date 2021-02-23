c44b9a7fc138b050a496f68e97198212cf7273f3
Piotr Grzyb <piotrgrzyb@gmail.com>
2021-02-23 23:04:04 +0100

    updated wednesday intro

--- components/Tabs.js
+++ components/Tabs.js
@@ -34,7 +34,7 @@ class Tabs extends React.Component {
           date: 'Feb 24, 2021',
           title: 'Środa',
           heading: 'Przerwa od nauki?',
-          intro: 'W prawdzie nie będziesz już co chwilę nazywana Królową, ale i bez tego ciąg dalszy kampanii jest ultra zajebiaszczy :D',
+          intro: 'W prawdzie nie będziesz już co chwilę nazywana Królową, ale i bez tego ciąg dalszy kampanii jest ultra zajebiaszczy :D PROTOSI TEŻ CHCĄ BYĆ KOCHANI!!! <br /> Kodzix: <strong class="text-white">XXXX-XXXX-XXXX-XXXX</strong>',
           svg: `<svg class="w-12 h-12" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
         </svg>`
@@ -87,8 +87,7 @@ class Tabs extends React.Component {
                     <Countdown date={date} className='text-xl font-bold'>
                       <div>                      
                         <h3 class="font-medium title-font text-white mb-1 text-md">{day.heading}</h3>
-                        <p class="leading-relaxed">
-                          {day.intro}
+                        <p class="leading-relaxed" dangerouslySetInnerHTML={{__html: day.intro}}>
                         </p>
                       </div>
                     </Countdown>
